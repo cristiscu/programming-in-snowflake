8abfb6f6353d2297bfbba17868baf30dc9666998
cristiscu <cristiscu@gmail.com>
2023-11-01 13:17:36 -0700

    deploying app

--- sections/streamlit-web-app/app.py
+++ sections/streamlit-web-app/app.py
@@ -1,18 +1,38 @@
-import json
+import json, urllib.parse
 import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
+from io import StringIO
 import m2_hierarchical, m3_graphs, m4_charts, m5_animated
+
+@st.cache_data(show_spinner="Loading the CSV file...")
+def loadFile(filename):
+    return pd.read_csv(filename).convert_dtypes()
 
 st.set_page_config(layout="wide")
 st.title("Hierarchical Data Viewer")
 st.caption("Display your hierarchical data with charts and graphs.")
 
-# load source data file
-df = pd.read_csv("data/employee-manager.csv").convert_dtypes()
-idx_label, idx_parent = 0, 1
-labels = df[df.columns[idx_label]]
-parents = df[df.columns[idx_parent]]
+with st.sidebar:
+    # upload CSV file or use default one
+    uploaded_file = st.file_uploader(
+        "Upload a CSV file", type=["csv"], accept_multiple_files=False)
+    
+    filename = "data/employee-manager.csv"
+    if uploaded_file is not None:
+        filename = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+    df = loadFile(filename)
+    cols = list(df.columns)
+
+    # get child and parent column names and indices
+    fld_label = st.selectbox('Child Column Name', cols, index=0)
+    idx_label = cols.index(fld_label)
+    labels = df[df.columns[idx_label]]
+
+    fld_parent = st.selectbox('Parent Column Name', cols, index=1)
+    idx_parent = cols.index(fld_parent)
+    parents = df[df.columns[idx_parent]]
 
 # create tab control
 tabSource, tabFormat, tabGraph, tabChart, tabAnim = st.tabs(
@@ -20,7 +40,7 @@ tabSource, tabFormat, tabGraph, tabChart, tabAnim = st.tabs(
 
 # show source as data frame
 with tabSource:
-    st.dataframe(df)
+    st.dataframe(df, use_container_width=True)
 
 # show in another data format
 with tabFormat:
@@ -40,6 +60,8 @@ with tabFormat:
 # show as GraphViz graph
 with tabGraph:
     graph = m3_graphs.getEdges(df, idx_label, idx_parent)
+    url = f'http://magjac.com/graphviz-visual-editor/?dot={urllib.parse.quote(graph)}'
+    st.link_button("Visualize Online", url)
     st.graphviz_chart(graph)
 
 # show as Plotly chart
